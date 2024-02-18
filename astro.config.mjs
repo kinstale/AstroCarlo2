7f634f733eedab0ba1fdfb2dd8955160203a6195
carlodaniele <carlo.daniele@gmail.com>
2024-02-18 16:00:34 +0100

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+// SSR
+import node from '@astrojs/node';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
+});
