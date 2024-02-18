5a309d28810b92b941d8c38e2e5c9dbf5bd06083
carlodaniele <carlo.daniele@gmail.com>
2024-02-18 16:40:04 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 	output: 'server',
 	adapter: node({
